993bee038567643bd0def4c0c0e9abc8b82e247a
zdliuccit <335758663@qq.com>
2018-07-04 15:50:44 +0800

    feat: 模版升级，和npm包固化

--- app.config.js
+++ app.config.js
@@ -12,8 +12,10 @@ module.exports = {
     designSize: 750,
     // 最优尺寸： 当屏幕宽度大于720的时候，默认显示iphone plus 414尺寸的放缩，主要适配ipad等
     optimalSize: 414,
-    // 生产环境代码是否启用离线缓存 默认 false
+    // 生产环境代码是否启用H5离线缓存 默认 false
     offlineCaching: false,
+    // 启用cordova 默认 false，对应 @sweetui/sweet-mobile 的 $sweet.exec功能
+    enableCordova: false,
     // webpack的差异化配置
     webpack: {
         // 入口
@@ -38,7 +40,7 @@ module.exports = {
     // 换肤配置
     themeConfig: {
         key: 'SWEET-THEME-COLOR', // localStorage存储的key字段, key根据项目变化,不能为空
-        name: 'red', // 项目初始化设置的皮肤的name 为空默认肤色 正常来说不设置
-        f7name: 'red', // 项目初始化设置的framework7皮肤name 为空默认肤色 正常来说不设置
+        name: '', // 项目初始化设置的皮肤的name 为空默认肤色 正常来说不设置
+        f7name: '', // 项目初始化设置的framework7皮肤name 为空默认肤色 正常来说不设置
     }
 }
