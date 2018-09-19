74df1254a6ba0826f036eb155b0f648ba9ee73a3
zdliuccit <335758663@qq.com>
2018-09-19 13:46:44 +0800

    feat: 更新模版

--- src/app.vue
+++ src/app.vue
@@ -2,7 +2,7 @@
     <f7-app :params="framework7">
         <f7-statusbar></f7-statusbar>
         <f7-panel left cover>
-            <f7-view url="/panel-left/"></f7-view>
+            <f7-view url="/panel-left/" links-view=".view-main"></f7-view>
         </f7-panel>
         <f7-panel right reveal>
             <f7-view url="/panel-right/"></f7-view>
